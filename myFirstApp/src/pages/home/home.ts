4f815be3f96b8691a620be72d5b8cf39165173b2
Kristofer Knutsen <Knutsen@KrisMac.local>
2019-01-17 14:21:41 +0200

    Task A done, commited and pushed

--- myFirstApp/src/pages/home/home.ts
+++ myFirstApp/src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Pic } from '../../interfaces/pic';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+  picArray: Pic[] = [];
+  mediaPath = 'assets/json/test.json';
 
-  constructor(public navCtrl: NavController) {
+  constructor(private http: HttpClient, public navCtrl: NavController) {
 
   }
 
+  ngOnInit() {
+    this.getImages();
+  }
+
+  getImages() {
+    this.http.get<Pic[]>(this.mediaPath).subscribe(
+      (response: Pic[]) => {
+        this.picArray = response;
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
+  }
+
+  /*
+  showImage(image){
+    this.photoViewer.show(image);
+
+    , private photoViewer: PhotoViewer
+  }
+  */
+
 }
+
+
